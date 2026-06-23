d071025a09742c440fbee6ff1a8fb879d01d54bb
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-23 12:28:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/CandidateProfile.java
+++ backend/src/main/java/com/example/demo/entity/CandidateProfile.java
@@ -18,5 +18,57 @@ public class CandidateProfile {
 
     private Integer yearsExperience;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public SystemUser getUser() {
+        return user;
+    }
+
+    public void setUser(SystemUser user) {
+        this.user = user;
+    }
+
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+
+    public void setResumeUrl(String resumeUrl) {
+        this.resumeUrl = resumeUrl;
+    }
+
+    public String getPrimarySkill() {
+        return primarySkill;
+    }
+
+    public void setPrimarySkill(String primarySkill) {
+        this.primarySkill = primarySkill;
+    }
+
+    public Integer getYearsExperience() {
+        return yearsExperience;
+    }
+
+    public void setYearsExperience(Integer yearsExperience) {
+        this.yearsExperience = yearsExperience;
+    }
+
+    public CandidateProfile() {
+    }
+
+    public CandidateProfile(Long id, SystemUser user, String resumeUrl, String primarySkill, Integer yearsExperience) {
+        this.id = id;
+        this.user = user;
+        this.resumeUrl = resumeUrl;
+        this.primarySkill = primarySkill;
+        this.yearsExperience = yearsExperience;
+    }
+
+    
     
 }
--- backend/src/main/java/com/example/demo/entity/InterviewSession.java
+++ backend/src/main/java/com/example/demo/entity/InterviewSession.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class InterviewSession {
     
+    private Long id
 }
