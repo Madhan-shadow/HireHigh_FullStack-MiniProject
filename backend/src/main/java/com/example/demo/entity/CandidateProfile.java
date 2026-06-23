a7a02e930756be1b4104f1bde6392bdff7be12cb
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-23 12:23:34 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/CandidateProfile.java
+++ backend/src/main/java/com/example/demo/entity/CandidateProfile.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class CandidateProfile {
+    @Id
+    private Long id;
+
+    private String user;
     
 }
