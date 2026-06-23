2b7ebbd5da37a61404cdc7895d9c9b6b42c09664
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-23 12:47:34 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/JobPosting.java
+++ backend/src/main/java/com/example/demo/entity/JobPosting.java
@@ -1,9 +1,19 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class JobPosting {
     
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String department;
+
+    private String description;
+
     
 }
