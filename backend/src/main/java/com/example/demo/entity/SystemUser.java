c869d2c89758562ce1186db18f1fd49bd6a8bdc4
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-24 07:24:12 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SystemUser.java
+++ backend/src/main/java/com/example/demo/entity/SystemUser.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 
 @Entity
 public class SystemUser {
@@ -11,5 +12,75 @@ public class SystemUser {
 
     private String username;
 
-    private String 
+    @Enc
+    private String password;
+
+    private String fullname;
+
+    private String role;
+
+    @Email
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public SystemUser() {
+    }
+
+    public SystemUser(Long id, String username, String password, String fullname, String role, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.role = role;
+        this.email = email;
+    }
+
+    
 }
