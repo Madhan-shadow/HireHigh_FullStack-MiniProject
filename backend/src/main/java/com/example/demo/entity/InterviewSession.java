a86046867565f527c350437215af363f779d16c9
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-25 10:57:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/InterviewSession.java
+++ backend/src/main/java/com/example/demo/entity/InterviewSession.java
@@ -4,14 +4,15 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="InterviewSession")
 public class InterviewSession {
     
     @Id
