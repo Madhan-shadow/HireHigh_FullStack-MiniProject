83f9a250d3d2bc9e046d0321abf4e799425caff9
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-25 12:12:03 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/JobApplication.java
+++ backend/src/main/java/com/example/demo/entity/JobApplication.java
@@ -1,97 +1,97 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.PrePersist;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name="JobApplication")
-public class JobApplication {
+// @Entity
+// @Table(name="JobApplication")
+// public class JobApplication {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY) 
-    @JoinColumn(name = "job_id", nullable = false)
-    private JobPosting job;
+//     @ManyToOne(fetch = FetchType.LAZY) 
+//     @JoinColumn(name = "job_id", nullable = false)
+//     private JobPosting job;
 
-    @ManyToOne(fetch = FetchType.LAZY) 
-    @JoinColumn(name = "candidate_id",nullable = false)
-    private CandidateProfile candidate;
+//     @ManyToOne(fetch = FetchType.LAZY) 
+//     @JoinColumn(name = "candidate_id",nullable = false)
+//     private CandidateProfile candidate;
 
-    @Column(name = "current_Stage", nullable = false)
-    private String currentStage;
+//     @Column(name = "current_Stage", nullable = false)
+//     private String currentStage;
 
-    @Column(name = "applied_at",nullable = false)
-    private LocalDateTime appliedAt;
+//     @Column(name = "applied_at",nullable = false)
+//     private LocalDateTime appliedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        appliedAt = LocalDateTime.now();
-    }
+//     @PrePersist
+//     protected void onCreate() {
+//         appliedAt = LocalDateTime.now();
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public JobPosting getJob() {
-        return job;
-    }
+//     public JobPosting getJob() {
+//         return job;
+//     }
 
-    public void setJob(JobPosting job) {
-        this.job = job;
-    }
+//     public void setJob(JobPosting job) {
+//         this.job = job;
+//     }
 
-    public CandidateProfile getCandidate() {
-        return candidate;
-    }
+//     public CandidateProfile getCandidate() {
+//         return candidate;
+//     }
 
-    public void setCandidate(CandidateProfile candidate) {
-        this.candidate = candidate;
-    }
+//     public void setCandidate(CandidateProfile candidate) {
+//         this.candidate = candidate;
+//     }
 
-    public String getCurrentStage() {
-        return currentStage;
-    }
+//     public String getCurrentStage() {
+//         return currentStage;
+//     }
 
-    public void setCurrentStage(String currentStage) {
-        this.currentStage = currentStage;
-    }
+//     public void setCurrentStage(String currentStage) {
+//         this.currentStage = currentStage;
+//     }
 
-    public LocalDateTime getAppliedAt() {
-        return appliedAt;
-    }
+//     public LocalDateTime getAppliedAt() {
+//         return appliedAt;
+//     }
 
-    public void setAppliedAt(LocalDateTime appliedAt) {
-        this.appliedAt = appliedAt;
-    }
+//     public void setAppliedAt(LocalDateTime appliedAt) {
+//         this.appliedAt = appliedAt;
+//     }
 
-    public JobApplication() {
-    }
+//     public JobApplication() {
+//     }
 
-    public JobApplication(Long id, JobPosting job, CandidateProfile candidate, String currentStage,
-            LocalDateTime appliedAt) {
-        this.id = id;
-        this.job = job;
-        this.candidate = candidate;
-        this.currentStage = currentStage;
-        this.appliedAt = appliedAt;
-    }
+//     public JobApplication(Long id, JobPosting job, CandidateProfile candidate, String currentStage,
+//             LocalDateTime appliedAt) {
+//         this.id = id;
+//         this.job = job;
+//         this.candidate = candidate;
+//         this.currentStage = currentStage;
+//         this.appliedAt = appliedAt;
+//     }
 
 
 
-}
+// }
