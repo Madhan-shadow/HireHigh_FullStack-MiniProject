fe003b58b69bb575d34e23696b6497c5dbb68bf1
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 21:44:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/dummyController.java
+++ backend/src/main/java/com/example/demo/controller/dummyController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +25,7 @@ public class dummyController {
             return ser.ct(dmy);
         
     }
+
+    @GetMapping("/fetch")
+    public List<
 }
