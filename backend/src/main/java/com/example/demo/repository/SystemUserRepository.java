d03bad56ef08462d6edceea464f9c86c6a540f14
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 12:17:58 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SystemUserRepository extends JpaRepository{
+public class SystemUserRepository extends JpaRepositoriesAutoConfiguration{
     
 }
