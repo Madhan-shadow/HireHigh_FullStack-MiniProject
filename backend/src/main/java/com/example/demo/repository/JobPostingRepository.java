d1cf8a1eb312d00913deabe08c466544ecd77c61
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 12:04:28 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/JobPostingRepository.java
+++ backend/src/main/java/com/example/demo/repository/JobPostingRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobPostingRepository extends JpaRepository{
+public interface JobPostingRepository extends JpaRepository<Entity, Long>{
     
 }
