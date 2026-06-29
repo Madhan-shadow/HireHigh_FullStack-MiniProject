535f60cce95feb3b60f78fa72f5bbb09c275191a
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 21:58:23 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/dummyController.java
+++ backend/src/main/java/com/example/demo/controller/dummyController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class dummyController {
     }
 
     @GetMapping("/fetchById")
-    public Optional<dummy> fetchById(){
-        return ser.fetchbyid(id);
+    public Optional<dummy> fetchById(@PathVariable id Long){
+        return ser.fetchbyid();
     }
 }
