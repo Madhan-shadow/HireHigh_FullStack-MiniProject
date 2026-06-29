58e6b0c12a3c4cd8225d2fc521ae1028dfcffcb6
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 21:35:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/dummyController.java
+++ backend/src/main/java/com/example/demo/controller/dummyController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.dummy;
 import com.example.demo.service.dummyServices;
 
 @RestController
+@RequestMapping("/api")
 public class dummyController {
     
     @Autowired
     dummyServices ser;
-    
+
+    @PostMapping("/create")
+    public dummy create
 }
