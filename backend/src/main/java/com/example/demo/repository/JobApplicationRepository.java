3621074438d4aadffa0c9af4ea0a80cef703b3ec
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 12:19:28 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/JobApplicationRepository.java
+++ backend/src/main/java/com/example/demo/repository/JobApplicationRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repository;
 
-import javax.swing.text.html.parser.Entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.SystemUser;
+
 @Repository
-public class SystemUserRepository extends JpaRepositoriesAutoConfiguration{
+public interface SystemUserRepository extends JpaRepository<SystemUser,Long> {
+
     
 }
