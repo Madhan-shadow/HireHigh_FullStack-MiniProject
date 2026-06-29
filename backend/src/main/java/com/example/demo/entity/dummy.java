f95e5a9c3b5bc9d60b85a0f4f147572bdb875c6c
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 21:32:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/dummy.java
+++ backend/src/main/java/com/example/demo/entity/dummy.java
@@ -1,5 +1,19 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class dummy {
+    
+    @Id
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    
+
     
 }
