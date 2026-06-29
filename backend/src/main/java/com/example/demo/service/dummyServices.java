2b4784b83a8863daaa77080e55bdc301dd7a6c32
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-29 21:50:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/dummyServices.java
+++ backend/src/main/java/com/example/demo/service/dummyServices.java
@@ -22,7 +22,7 @@ public class dummyServices {
     public List<dummy> fetchdetail(){
         return rep.findAll();
     }
-    public Optional<dummy> fetchbyid(){
-        return rep.fetchbyid(dummy);
+    public Optional<dummy> fetchbyid(Long id){
+            return rep.findById(dummy);
     }
 }
