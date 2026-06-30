d29c3e20dac113c0e13aa22a001fe3883ed121a2
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-30 08:31:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/dummyServices.java
+++ backend/src/main/java/com/example/demo/service/dummyServices.java
@@ -29,7 +29,7 @@ public class dummyServices {
     public Optional<dummy> fetchbyid(@PathVariable @NonNull Long id){
             return rep.findById(id);
     }
-    public dummy delall(dummy dmy){
-        return rep.deleteAll(dmy);
+    public dummy delall(@RequestBody dummy dmy){
+        return rep.deleteAll();
     }
 }
