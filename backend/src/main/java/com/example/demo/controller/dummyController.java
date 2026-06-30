bd2519181bddf86216042388d9f19e39b0c05ca3
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-30 08:51:15 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/dummyController.java
+++ backend/src/main/java/com/example/demo/controller/dummyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +40,9 @@ public class dummyController {
     public Optional<dummy> fetchById(@PathVariable @NonNull Long id ){
         return ser.fetchbyid(id);
     }
+
+    @PutMapping("/update")
+    public String updatedata(@PathVariable Long id,)
     @DeleteMapping("/deleteall")
     public dummy deleteall(@RequestBody dummy dmy){
         return ser.delall(dmy);
