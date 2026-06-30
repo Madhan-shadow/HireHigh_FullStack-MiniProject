85b2d84206eb4ec3c18beb58eca30fd8e631a22c
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-30 09:46:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/dummyController.java
+++ backend/src/main/java/com/example/demo/controller/dummyController.java
@@ -50,7 +50,7 @@ public class dummyController {
 
    @DeleteMapping("/delete/{id}")
     public String deletedata(@PathVariable @NonNull Long id){
-        Optional<dummy> dy1= ser.fetchbyid(id);
-        return ser.delid(dy1,id);
+        Optional<dummy> dy= ser.fetchbyid(id);
+        return ser.delid(dy,id);
     }
 }
