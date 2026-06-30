f4742fa5f39d1685b7484ebb3d3cd193049fae70
MADHAN R <727824TUAD117@skct.edu.in>
2026-06-30 10:04:44 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/JobController.java
+++ backend/src/main/java/com/example/demo/controller/JobController.java
@@ -1,6 +1,33 @@
 package com.example.demo.controller;
 
-class JobController {
+import java.util.List;
+import java.util.Optional;
 
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.JobPosting;
+import com.example.demo.service.JobService;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @Autowired
+    JobService service;
+
+    @PostMapping("/create")
+    public JobPosting create(@RequestBody JobPosting job) {
+        return service.create(job);
+    }
+
+    @GetMapping("/fetch")
+    public List<JobPosting> fetch() {
+        return service.fetchAll();
+    }
+
+    @GetMapping("/fetchById/{id}")
+    public Optional<JobPosting> fetchById(@PathVariable Long id) {
+        return service.fetchById(id);
+    }
 }
