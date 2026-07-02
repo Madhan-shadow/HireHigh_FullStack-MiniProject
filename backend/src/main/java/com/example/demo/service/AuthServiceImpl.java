4fcb5b70b79628a003a023e996ab267ef02f9b58
MADHAN R <727824TUAD117@skct.edu.in>
2026-07-02 19:52:47 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.example.demo.dto.AuthRequestDto;
 import com.example.demo.dto.AuthResponseDto;
 import com.example.demo.dto.RegisterDto;
