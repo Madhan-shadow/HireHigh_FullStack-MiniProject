f07ec7994dda18ef3fe7e2b763d69ccc25395e52
MADHAN R <727824TUAD117@skct.edu.in>
2026-07-03 10:51:00 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/JobManagementServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/JobManagementServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.JobPosting;
