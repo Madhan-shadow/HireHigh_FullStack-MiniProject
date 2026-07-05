df90a7dbee092334695bf94bc725dbf07eddadb9
MADHAN R <727824TUAD117@skct.edu.in>
2026-07-05 17:15:53 +0530

    User commit

--- backend/src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,14 +17,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
     private final JwtService jwtService;
 
+    @Autowired
     private final UserDetailsService userDetailsService;
 
     @Override
