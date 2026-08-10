c73a7cd90e259023d70a27573c09976e900aed61
MADHAN R <727824TUAD117@skct.edu.in>
2026-08-10 14:19:32 +0530

    User commit

--- frontend/src/App1.jsx
+++ frontend/src/App1.jsx
@@ -4,6 +4,8 @@ export const App1 = () => {
   return (
     <div align = "center">
         <h1>It's Worked</h1>
+        <h1>Prassanna   ❤️❤️❤️❤️❤️❤️  Anni</h1>
+        <h1> mani </h1>
         </div>
   )
 }
