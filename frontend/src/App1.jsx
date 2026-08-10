fa85ad24536d888f860a214b223186e234be889e
MADHAN R <727824TUAD117@skct.edu.in>
2026-08-10 14:28:38 +0530

    User commit

--- frontend/src/App1.jsx
+++ frontend/src/App1.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export const App1 = () => {
-    let a=999
+    let a=100
   return (
     <div align = "center">
-        <h1>prasanna is</h1>
-        <h1>{a}</h1>
+        <h1>It's Worked</h1>
+        <h1>{a}%</h1>
         </div>
   )
 }
