74f07c8721a8a1ac2a78b8f7815dfc4514e9b613
MADHAN R <727824TUAD117@skct.edu.in>
2026-08-22 12:39:08 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,39 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
-import { Provider, useDispatch, useSelector } from "react-redux";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet
+} from "react-router-dom";
+
+import {
+  Provider,
+  useDispatch,
+  useSelector
+} from "react-redux";
 
 import store from "./store";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 import JobList from "./components/jobs/JobList";
 import ApplicationList from "./components/applications/ApplicationList";
+import Navbar from "./components/layout/Navbar";
+
 import { hydrate } from "./store/slices/authSlice";
 
 function ProtectedRoute() {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated } =
+    useSelector((state) => state.auth);
 
   return isAuthenticated ? (
     <Outlet />
   ) : (
-    <Navigate to="/login" replace />
+    <Navigate
+      to="/login"
+      replace
+    />
   );
 }
 
@@ -28,17 +46,52 @@ function AppContent() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Navbar />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/jobs" element={<JobList />} />
-          <Route path="/applications" element={<ApplicationList />} />
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          element={<ProtectedRoute />}
+        >
+          <Route
+            path="/jobs"
+            element={<JobList />}
+          />
+
+          <Route
+            path="/applications"
+            element={<ApplicationList />}
+          />
         </Route>
 
-        <Route path="/" element={<Navigate to="/jobs" replace />} />
-        <Route path="*" element={<Navigate to="/jobs" replace />} />
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to="/jobs"
+              replace
+            />
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to="/jobs"
+              replace
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
