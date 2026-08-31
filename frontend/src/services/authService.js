6c43521b0242280b60a0f2126cb8d83cd843648b
MADHAN R <727824TUAD117@skct.edu.in>
2026-08-31 12:49:18 +0530

    User commit

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,13 +2,11 @@ import api from './api';
 
 const login = async (credentials) => {
   const response = await api.post('/auth/login', credentials);
-
   return response.data;
 };
 
 const register = async (userData) => {
   const response = await api.post('/auth/register', userData);
-
   return response.data;
 };
 
