cb1b57982620f6894bef69c53db40c2d639639a2
MADHAN R <727824TUAD117@skct.edu.in>
2026-08-31 13:26:37 +0530

    User commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider, useSelector } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Login from './components/Login';
 import Register from './components/Register';
 import JobList from './components/jobs/JobList';
 import ApplicationList from './components/applications/ApplicationList';
-import ProtectedRoute from './components/common/ProtectedRoute';
+import './App.css';
 
-function App() {
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+const Home = () => (
+  <div className="page-container">
+    <h1>Welcome to HireHigh</h1>
+    <p>Talent acquisition pipeline management, end to end.</p>
+  </div>
+);
+
+function AppRoutes() {
   return (
-    <div className="app">
-
+    <>
       <Navbar />
-
       <Routes>
-
-        <Route
-          path="/"
-          element={<Navigate to="/jobs" replace />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/jobs"
           element={
@@ -39,30 +40,27 @@ function App() {
             </ProtectedRoute>
           }
         />
-
         <Route
           path="/applications"
           element={
-            <ProtectedRoute
-              allowedRoles={[
-                'RECRUITER',
-                'TA_LEAD',
-                'HIRING_MANAGER'
-              ]}
-            >
+            <ProtectedRoute>
               <ApplicationList />
             </ProtectedRoute>
           }
         />
-
-        <Route
-          path="*"
-          element={<Navigate to="/jobs" replace />}
-        />
-
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+    </>
+  );
+}
 
-    </div>
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
