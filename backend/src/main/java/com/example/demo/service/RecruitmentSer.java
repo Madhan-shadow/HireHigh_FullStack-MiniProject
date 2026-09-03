224508deee7f352b3e97a5adff2d32636ecaaf2b
MADHAN R <727824TUAD117@skct.edu.in>
2026-09-03 19:28:05 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/RecruitmentSer.java
+++ backend/src/main/java/com/example/demo/service/RecruitmentSer.java
@@ -17,7 +17,7 @@ import com.example.demo.repository.JobPostingRepository;
 import com.example.demo.repository.SystemUserRepository;
 
 @Service
-public class RecruitmentServiceImpl implements RecruitmentService {
+public class RecruitmentSer     viceImpl implements RecruitmentService {
 
     @Autowired
     private JobApplicationRepository applicationRepository;
