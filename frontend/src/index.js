5a2c2d16b5f38116c8356f1354381433dea52a0e
MADHAN R <727824TUAD117@skct.edu.in>
2026-09-04 15:59:22 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {
-  Provider,
-} from "react-redux";
-
-import {
-  BrowserRouter,
-} from "react-router-dom";
-
 import App from "./App";
-import { store } from "./store";
 
 import "./styles.css";
 
@@ -21,10 +12,6 @@ const root =
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
