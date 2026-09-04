9d3f84abf238a2d6cde4fc59e69c3e10bf95a8b1
MADHAN R <727824TUAD117@skct.edu.in>
2026-09-04 15:54:52 +0530

    User commit

---
+++ backend/src/main/java/com/example/demo/dto/CandidateProfileDto.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+public class CandidateProfileDto {
+
+    private String resumeUrl;
+    private String primarySkill;
+    private Integer yearsExperience;
+
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+
+    public void setResumeUrl(String resumeUrl) {
+        this.resumeUrl = resumeUrl;
+    }
+
+    public String getPrimarySkill() {
+        return primarySkill;
+    }
+
+    public void setPrimarySkill(String primarySkill) {
+        this.primarySkill = primarySkill;
+    }
+
+    public Integer getYearsExperience() {
+        return yearsExperience;
+    }
+
+    public void setYearsExperience(Integer yearsExperience) {
+        this.yearsExperience = yearsExperience;
+    }
+}
