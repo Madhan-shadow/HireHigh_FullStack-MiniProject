13140aba79e93ce480d9a48fee26041eb7bc73ab
MADHAN R <727824TUAD117@skct.edu.in>
2026-09-12 11:26:20 +0530

    User commit

---
+++ frontend/src/App1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App1 = () => {
+    let a=100
+    // let[count,setCount]=useState(0);
+  return (
+    <div align = "center">
+        <h1 style={{color:"green"}}>It's Worked</h1>
+        <h1>{a}/10</h1>
+        </div>
+  ) 
+}
+export default App1
